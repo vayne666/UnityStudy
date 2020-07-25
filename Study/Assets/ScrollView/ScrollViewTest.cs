f35962eb6acf7f9cd775201cd8c59a7f87bdcbde
vayne666 <vayne666@qq.com>
2020-07-25 18:31:48 +0800

    test

--- Study/Assets/ScrollView/ScrollViewTest.cs
+++ Study/Assets/ScrollView/ScrollViewTest.cs
@@ -1,22 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+
+using UnityEngine.UI;
 using UnityEngine;
 
-public class ScrollViewTest : MonoBehaviour
-{
+public class ScrollViewTest : MonoBehaviour {
     // Start is called before the first frame update
+    
     UIWarpContent uIWarpContent;
-    void Start()
-    {
-        uIWarpContent=this.GetComponent<UIWarpContent>();
-        //uIWarpContent.PerCreatItem(100,false);
-        uIWarpContent.Init(10);
-        
+    void Awake() {
+        uIWarpContent = this.GetComponent<UIWarpContent>();
+        uIWarpContent.onInitializeItem += onInitializeItem;
+        //uIWarpContent.PerCreatItem(100, false);
+        //uIWarpContent.viewCount = 5;
+        ////uIWarpContent.Init(10);
+        //uIWarpContent.cellHeight = 100;
+        //uIWarpContent.cellWidth = 100;
+        //uIWarpContent.EndInterval = 40;
+        //uIWarpContent.onInitializeItem = onInitializeItem;
+        //uIWarpContent.viewCount = 10;
+        //uIWarpContent.maxPerLine = 2;
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+
+    }
+
+    private void OnGUI() {
+        if (GUILayout.Button("dwadwadad")) {
+            uIWarpContent.Init(100);
+        }
+    }
+
+
+    public void onInitializeItem(GameObject go, int idx) {
+        Debug.LogError(idx);
         
+        var t = go.transform.Find("Value").GetComponent<Text>();
+
+        t.text = idx.ToString();
     }
 }
